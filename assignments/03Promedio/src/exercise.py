0a779545693c8e5ebdfaa52067ea8e6f9ecfc1b1
A01635561 <a01635561@itesm.mx>
2021-08-20 15:30:03 +0000

    commit on "main"

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,6 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    
+    num1 = float(input("Calificación de la materia: "))
+    num2 = float(input("Calificación de la materia: "))
+    num3 = float(input("Calificación de la materia: "))
+    num4 = float(input("Calificación de la materia: "))
+    promedio = num1 + num2 + num3 + num4
+    Cal_Final = promedio / 4
+    print("El promedio es:", Cal_Final)
+   
 
 if __name__ == '__main__':
     main()
